1569f3f1cc01fcffaf7ba8ae15151f59f8b52545
xavierotazuUAB <xavier.otazu@uab.cat>
2025-09-22 12:51:02 +0200

    Primera versió del codi de test
    Definim un primer codi de test, sense implementar cap part del codi desenvolupat. Per aquest motiu, el IDE pot ser que ens indiqui que tenim errors de sintaxi, doncs no troba la implementació dels mètodes que apareixen en el codi de test.

--- src/money/MoneyTest.java
+++ src/money/MoneyTest.java
@@ -7,8 +7,16 @@ import org.junit.jupiter.api.Test;
 class MoneyTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testSimpleAdd() {
+		// [12 CHF] + [14 CHF] == [26 CHF]
+		Money f12CHF   = new Money(12,”CHF”);
+		Money f14CHF   = new Money(14,”CHF”);
+		Money expected = new Money(26, "CHF");
+		Money result   = f12CHF.add(f14CHF);
+
+		assertTrue(result.amount()==expected.amount());
+		assertTrue(result.currency()==expected.currency());
+
 	}
 
 }
